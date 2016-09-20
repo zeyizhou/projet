ab7dcc181d7c6a0ad6d70a62587e1069be2f89c8
sebastien.gadot <sebastien.gadot@utbm.fr>
2016-09-20 10:07:18 -0400

    commit du date majorite

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+  def major?
+    birthday < Time.now - 18.years
+  end
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'minor student' do
+    bill = Student.find_by_name('bill')
+    assert_equal bill.major?, false    
+  end
+  
+  test 'major student' do
+    joe = Student.find_by_name('joe')
+    assert_equal joe.major?, true
+  end
 end
