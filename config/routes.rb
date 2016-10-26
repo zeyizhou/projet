c99d5111c317e586fe38088d27a91fc5fe6ee627
zzy28101994 <zzy28101994@gmail.com>
2016-10-26 19:00:30 -0400

    sea

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :students
   
   resources :forecasts
-  
-  resources :angular
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
